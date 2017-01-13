3ad7ccaf313a0ac5f41a05af3b39fc0ba2abc178
Ru Tan <jutan2012@gmail.com>
2017-01-13 09:34:30 +0100

    Ajout <form>

---
+++ Exer1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exer 1</title>
+</head>
+<body>
+	<form>
+		<label>User Agent</label>
+		<input type="text" name="userAgent">
+		<label>Adresse IP</label>
+		<input type="text" name="adresseIp">
+		<label>Nom du Serveur</label>
+		<input type="text" name="nomduServeur">
+		<input type="submit" value="Envoyer">
+	</form>
+</body>
+</html>
