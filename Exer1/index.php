512cb149da8219996f422fd1dd3642a3b7a22e3b
Ru Tan <jutan2012@gmail.com>
2017-01-13 15:43:56 +0100

    Ajout $_SERVER["HTTP_USER_AGENT"]; $_SERVER["REMOTE_ADDR"];  $_SERVER["SERVER_NAME"];

--- Exer1/index.php
+++ Exer1/index.php
@@ -7,15 +7,15 @@
 <body>
 	<form method="" action="">
 		<label for="userAgent">User Agent: </label>
-		<input type="text" name="userAgent"> 
+		<input type="text" name="userAgent" value="<?php echo $_SERVER["HTTP_USER_AGENT"];?>">
 		<br>
 		<br>
 		<label for="adresseIp">Adresse IP : </label>
-		<input type="text" name="adresseIp">
+		<input type="text" name="adresseIp" value="<?php echo $_SERVER["REMOTE_ADDR"] ?>">
 		<br>
 		<br>
 		<label for="nomduServeur">Nom du Serveur : </label>
-		<input type="text" name="nomduServeur">
+		<input type="text" name="nomduServeur" value="<?php echo $_SERVER["SERVER_NAME"] ?>">
 		<br>
 		<br>
 		<input type="submit" value="Envoyer">
